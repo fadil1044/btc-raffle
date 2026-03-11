603c36621c0e6d750b635c12a6788515ad732559
fadil1044 <126543452+fadil1044@users.noreply.github.com>
2026-03-12 01:10:37 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { WalletConnectProvider } from '@btc-vision/walletconnect'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WalletConnectProvider
-      theme="dark"
-      recommendedWallet="OP_WALLET"
-      supportedWallets={['OP_WALLET']}
-    >
-      <App />
-    </WalletConnectProvider>
+    <App />
   </React.StrictMode>,
 )
